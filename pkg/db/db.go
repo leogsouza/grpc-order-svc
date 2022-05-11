36c2dc971e531851852162669f7dac3bf1b36320
Leonardo Souza <leogsouza@gmail.com>
2022-05-11 17:29:56 -0300

    Implement order model and db handler

---
+++ pkg/db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"log"
+
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+)
+
+type Handler struct {
+	DB *gorm.DB
+}
+
+func Init(url string) Handler {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(&models.Order{})
+
+	return Handler{db}
+}
